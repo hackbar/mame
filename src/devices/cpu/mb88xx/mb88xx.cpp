203f4793bebc4a421b887fc9dc7cb0e614a5f6db
Mike Harris <mike.harris@gmail.com>
2021-01-27 19:59:22 -0800

    mb88xx: clarify interrupt line is 'logical', not voltage-based.

--- src/devices/cpu/mb88xx/mb88xx.cpp
+++ src/devices/cpu/mb88xx/mb88xx.cpp
@@ -368,7 +368,9 @@ int mb88_cpu_device::pla( int inA, int inB )
 
 void mb88_cpu_device::execute_set_input(int inputnum, int state)
 {
-	/* on rising edge trigger interrupt */
+	/* On rising edge trigger interrupt.
+	 * Note this is a logical level, the actual pin is high-to-low voltage
+	 * triggered. */
 	if ( (m_pio & 0x04) && !m_nf && state != CLEAR_LINE )
 	{
 		m_pending_interrupt |= INT_CAUSE_EXTERNAL;
